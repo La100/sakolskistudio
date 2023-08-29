8a6a0a82dd450c1fa2fb576e4958669421978d3c
La100 <stolamarcin@gmail.com>
2023-08-29 13:59:04 +0200

    pixel

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,5 +1,5 @@
 'use client'
-
+import Script from 'next/script'
 import {
   createContext,
   useContext,
@@ -51,6 +51,30 @@ function Header({
   let { logoHovered, setLogoHovered } = useContext(RootLayoutContext)
 
   return (
+    <>
+    <Script
+id="fb-pixel"
+strategy="afterInteractive"
+dangerouslySetInnerHTML={{
+__html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '270175679293092');
+fbq('track', 'PageView');
+`,
+}}
+/>
+  
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=270175679293092&ev=PageView&noscript=1"
+/></noscript>
+
     <Container>
       <div className="flex items-center justify-between">
         <Link
@@ -91,6 +115,7 @@ function Header({
         </div>
       </div>
     </Container>
+    </>
   )
 }
 
